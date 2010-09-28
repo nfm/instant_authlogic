8fe9298171e1d7cf42fd759c66929aba1e37422f
Nicholas Firth-McCoy <nfm@scout.(none)>
2010-09-28 15:23:55 +1000

    Setup install.rb to copy files from lib/installed_files/ into the app

--- install.rb
+++ install.rb
@@ -1,1 +1,16 @@
-# Install hook code here
+puts "Copying files..."
+
+src_dir = File.join(File.dirname(__FILE__), "lib", "installed_files")
+
+FileUtils.copy(File.join(src_dir, "controllers", "users_controller.rb"), "#{RAILS_ROOT}/app/controllers/")
+FileUtils.copy(File.join(src_dir, "controllers", "user_sessions_controller.rb"), "#{RAILS_ROOT}/app/controllers/")
+
+FileUtils.copy(File.join(src_dir, "models", "user.rb"), "#{RAILS_ROOT}/app/models/")
+FileUtils.copy(File.join(src_dir, "models", "user_session.rb"), "#{RAILS_ROOT}/app/models/")
+
+FileUtils.copy(File.join(src_dir, "views", "users/"), "#{RAILS_ROOT}/app/views/")
+
+timestamp = Time.now.strftime('%Y%m%d%H%M%S')
+FileUtils.copy(File.join(src_dir, "migrate", "create_users.rb"), "#{RAILS_ROOT}/db/migrate/#{timestamp}_create_users.rb")
+
+puts "Files copied - installation complete!"
